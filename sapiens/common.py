5350079631db99e09012beac1fb8888df30200db
Ibai Gorordo <43162939+ibaiGorordo@users.noreply.github.com>
2024-08-29 21:01:50 +0900

    Add model download

--- sapiens/common.py
+++ sapiens/common.py
@@ -1,6 +1,36 @@
+import os
+import shutil
 from typing import List
+from enum import Enum
+from huggingface_hub import hf_hub_download
 
 from torchvision import transforms
+
+
+class TaskType(Enum):
+    DEPTH = "depth"
+    NORMAL = "normal"
+    SEG = "seg"
+
+
+def download_hf_model(model_name: str, task_type: TaskType, model_dir: str = 'models'):
+    if not os.path.exists(model_dir):
+        os.makedirs(model_dir)
+
+    path = model_dir + "/" + model_name
+    if os.path.exists(path):
+        return path
+
+    model_version = "_".join(model_name.split("_")[:2])
+    repo_id = "facebook/sapiens"
+    subdirectory = f"sapiens_lite_host/torchscript/{task_type.value}/checkpoints/{model_version}"
+
+    hf_hub_download(repo_id=repo_id, filename=model_name, subfolder=subdirectory, local_dir=model_dir)
+
+    os.rename(model_dir + "/" + subdirectory + "/" + model_name, path)
+    shutil.rmtree(model_dir + "/" + subdirectory.split("/")[0])
+
+    return path
 
 
 def create_preprocessor(input_size: tuple[int, int],
