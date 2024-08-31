5647f0761a89d42810212ead12fecb6291245d54
Ibai Gorordo <43162939+ibaiGorordo@users.noreply.github.com>
2024-08-31 17:00:38 +0900

    Update __init__.py

---
+++ sapiens/__init__.py
@@ -0,0 +1,4 @@
+from .predictor import SapiensPredictor, SapiensConfig
+from .normal import SapiensNormal, SapiensNormalType
+from .segmentation import SapiensSegmentation, SapiensSegmentationType
+from .depth import SapiensDepth, SapiensDepthType
