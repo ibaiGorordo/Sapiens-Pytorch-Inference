1732e9d3be895bf2c5b6af29783e9cca98e3dfd8
Ibai Gorordo <43162939+ibaiGorordo@users.noreply.github.com>
2024-08-31 22:16:36 +0900

    Add progress bar for model download

--- sapiens/common.py
+++ sapiens/common.py
@@ -1,8 +1,10 @@
 import os
 import shutil
 from typing import List
+import requests
+from tqdm import tqdm
 from enum import Enum
-from huggingface_hub import hf_hub_download
+from huggingface_hub import hf_hub_download, hf_hub_url
 
 from torchvision import transforms
 
@@ -13,6 +15,26 @@ class TaskType(Enum):
     SEG = "seg"
 
 
+def download(url: str, filename: str):
+    with open(filename, 'wb') as f:
+        with requests.get(url, stream=True) as r:
+            r.raise_for_status()
+            total = int(r.headers.get('content-length', 0))
+
+            # tqdm has many interesting parameters. Feel free to experiment!
+            tqdm_params = {
+                'total': total,
+                'miniters': 1,
+                'unit': 'B',
+                'unit_scale': True,
+                'unit_divisor': 1024,
+            }
+            with tqdm(**tqdm_params) as pb:
+                for chunk in r.iter_content(chunk_size=8192):
+                    pb.update(len(chunk))
+                    f.write(chunk)
+
+
 def download_hf_model(model_name: str, task_type: TaskType, model_dir: str = 'models'):
     if not os.path.exists(model_dir):
         os.makedirs(model_dir)
@@ -21,18 +43,15 @@ def download_hf_model(model_name: str, task_type: TaskType, model_dir: str = 'mo
     if os.path.exists(path):
         return path
 
-    print(f"Model {model_name} not found, downloading from Hugging Face Hub..."
-          f"It will take a while, you will be notified when it's done.")
+    print(f"Model {model_name} not found, downloading from Hugging Face Hub...")
 
     model_version = "_".join(model_name.split("_")[:2])
     repo_id = "facebook/sapiens"
     subdirectory = f"sapiens_lite_host/torchscript/{task_type.value}/checkpoints/{model_version}"
 
-    hf_hub_download(repo_id=repo_id, filename=model_name, subfolder=subdirectory, local_dir=model_dir)
-
-    os.rename(model_dir + "/" + subdirectory + "/" + model_name, path)
-    shutil.rmtree(model_dir + "/" + subdirectory.split("/")[0])
-
+    # hf_hub_download(repo_id=repo_id, filename=model_name, subfolder=subdirectory, local_dir=model_dir)
+    url = hf_hub_url(repo_id=repo_id, filename=model_name, subfolder=subdirectory)
+    download(url, path)
     print("Model downloaded successfully to", path)
 
     return path
